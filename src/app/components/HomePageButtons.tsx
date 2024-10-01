5921161a7939fd0acacb31ce758658e7d7ef5f5d
Frankie Rodriguez <rfrankie@gmail.com>
2024-10-01 04:31:40 -0400

    button styling fixes

--- src/app/components/HomePageButtons.tsx
+++ src/app/components/HomePageButtons.tsx
@@ -25,7 +25,7 @@ export const HomePageButtons = () => {
                     onClick={handleAppRedirect}>Use the App
             </button>
             <button
-                className="bg-white hover:bg-gray-500 text-gray-800 hover:text-white font-semibold py-2 px-4 rounded-lg shadow transition ease-in-out duration-200 m5x       "
+                className="bg-white hover:bg-gray-500 text-gray-800 hover:text-white font-semibold py-2 px-4 rounded-lg shadow transition ease-in-out duration-200 m-5"
                 onClick={handleDemoRedirect}>Use the Demo
             </button>
             </div>
